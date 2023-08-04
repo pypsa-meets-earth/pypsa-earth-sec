e2ccbf06254fb45e5e45c6c3a756e29197b5d988
Hazem-IEG <hazem.abdel-khalek@ieg.fraunhofer.de>
2023-08-04 14:55:45 +0200

    adding the slurm script

---
+++ submit_2030.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --error='job-%j-error.out'
+#SBATCH --output='job-%j-out.out'
+#SBATCH --export=ALL
+#SBATCH --chdir=/nfs/home/cas96273/Thesis_Code/pypsa-earth-sec
+
+module purge
+module load Anaconda3
+module load Java
+source activate /nfs/home/cas96273/.conda/envs/pypsa-earth
+
+export GRB_LICENSE_FILE=/nfs/home/cas96273/gurobi.lic
+
+rm /nfs/home/cas96273/Thesis_Code/pypsa-earth/networks/elec.nc
+rm /nfs/home/cas96273/Thesis_Code/pypsa-earth/networks/elec_s.nc
+
+cp config.pypsa-earth_conservative_2030.yaml config.pypsa-earth.yaml
+cp config_2030_cons.yaml config.yaml
+
+snakemake -j 32 plot_summary
+
+cp config_2030_cons_Qs.yaml config.yaml
+
+snakemake -j 32 plot_summary
+
+
+rm /nfs/home/cas96273/Thesis_Code/pypsa-earth/networks/elec.nc
+rm /nfs/home/cas96273/Thesis_Code/pypsa-earth/networks/elec_s.nc
+
+cp config.pypsa-earth_realistic_2030.yaml config.pypsa-earth.yaml
+cp config_2030_real.yaml config.yaml
+
+snakemake -j 32 plot_summary
+
+cp config_2030_real_Qs.yaml config.yaml
+
+snakemake -j 32 plot_summary
+
+
+rm /nfs/home/cas96273/Thesis_Code/pypsa-earth/networks/elec.nc
+rm /nfs/home/cas96273/Thesis_Code/pypsa-earth/networks/elec_s.nc
+
+cp config.pypsa-earth_optimistic_2030.yaml config.pypsa-earth.yaml
+cp config_2030_opt.yaml config.yaml
+
+snakemake -j 32 plot_summary
+
+cp config_2030_opt_Qs.yaml config.yaml
+
+snakemake -j 32 plot_summary
