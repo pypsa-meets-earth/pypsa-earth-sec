1b6785714bde3f51a1dd85bf577bda19e94d60b4
Anton Achhammer <anton.achhammer@oth-regensburg.de>
2024-06-02 09:18:42 +0200

    fix typerror in plot_clustered_network as well

--- scripts/prepare_gas_network.py
+++ scripts/prepare_gas_network.py
@@ -832,6 +832,8 @@ def plot_clustered_gas_network(pipelines, bus_regions_onshore):
     centroids["geometry"] = centroids["geometry"].centroid
     centroids["gadm_id"] = centroids["gadm_id"].apply(
         lambda id: three_2_two_digits_country(id[:3]) + id[3:]
+        if len(id[:3]) == 3
+        else id
     )
     gdf1 = pd.merge(
         pipelines, centroids, left_on=["bus0"], right_on=["gadm_id"], how="left"
